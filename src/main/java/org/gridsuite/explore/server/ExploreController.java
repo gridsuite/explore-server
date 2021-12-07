312cb1630f1923ca4ebb55c29e86a1696706fe3c
Abdelsalem <abdelsalem.hedhili@rte-france.com>
2021-12-07 11:59:23 +0100

    test jetty
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>

--- src/main/java/org/gridsuite/explore/server/ExploreController.java
+++ src/main/java/org/gridsuite/explore/server/ExploreController.java
@@ -142,7 +142,7 @@ public class ExploreController {
     @Operation(summary = "get element infos from ids given as parameters")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "The elements information")})
     public ResponseEntity<Mono<List<ElementAttributes>>> getElementsMetadata(@RequestParam("id") List<UUID> ids) {
-        System.out.println("NEW VERSION 2................................................................");
+        System.out.println("NEW VERSION 3................................................................");
         return ResponseEntity.ok().body(exploreService.getElementsMetadata(ids));
     }
 }
