ada62ad7682d1a051e3af4b993679dbcaad858e1
Seddik Yengui <seddik.yengui@rte-france.com>
2022-12-01 15:31:06 +0100

    fixes

--- src/main/java/org/gridsuite/explore/server/services/IDirectoryElementsService.java
+++ src/main/java/org/gridsuite/explore/server/services/IDirectoryElementsService.java
@@ -32,9 +32,7 @@ interface IDirectoryElementsService {
         Map<String, ElementAttributes> mapElementAttribute = lstElementAttribute.stream()
                 .collect(Collectors.toMap(e -> e.getElementUuid().toString(), Function.identity()));
         /* getting metadata from services */
-        List<UUID> uuids = lstElementAttribute.stream().map(ElementAttributes::getElementUuid).collect(Collectors.toList());
-        List<Map<String, Object>> metadataTest = uuids.stream().map(e -> Map.of("id", (Object) e)).collect(Collectors.toList());
-        List<Map<String, Object>> metadata = getMetadata(uuids);
+        List<Map<String, Object>> metadata = getMetadata(lstElementAttribute.stream().map(ElementAttributes::getElementUuid).collect(Collectors.toList()));
         return metadata.stream().map(metadataItem ->
                 populateMedataItem(mapElementAttribute.get(metadataItem.getOrDefault("id", "").toString()), metadataItem)
         ).collect(Collectors.toList());
