32b036cacfefd5f79739a2d953e82c7e985f947e
Jon Harper <jon.harper87@gmail.com>
2023-09-18 15:03:19 +0200

    remove dead code (#59)

--- src/main/java/org/gridsuite/explore/server/services/StudyService.java
+++ src/main/java/org/gridsuite/explore/server/services/StudyService.java
@@ -6,22 +6,15 @@
  */
 package org.gridsuite.explore.server.services;
 
-import org.gridsuite.explore.server.ExploreException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
-
-import static org.gridsuite.explore.server.ExploreException.Type.*;
 
 /**
  * @author Etienne Homer <etienne.homer at rte-france.com>
@@ -57,26 +50,6 @@ public class StudyService implements IDirectoryElementsService {
         headers.add(HEADER_USER_ID, userId);
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(
                 importParams, headers);
-        restTemplate.exchange(studyServerBaseUri + path, HttpMethod.POST, request, Void.class);
-    }
-
-    public void insertStudyWithCaseFile(UUID studyUuid, String userId, MultipartFile caseFile) {
-        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
-        String path = UriComponentsBuilder.fromPath(DELIMITER + STUDY_SERVER_API_VERSION +
-                "/studies?studyUuid={studyUuid}")
-                .buildAndExpand(studyUuid)
-                .toUriString();
-        try {
-            multipartBodyBuilder.part("caseFile", caseFile.getBytes())
-                    .filename(Objects.requireNonNull(caseFile.getOriginalFilename()));
-        } catch (IOException e) {
-            throw new ExploreException(IMPORT_CASE_FAILED);
-        }
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        headers.add(HEADER_USER_ID, userId);
-        HttpEntity<MultiValueMap<String, HttpEntity<?>>> request = new HttpEntity<>(
-                multipartBodyBuilder.build(), headers);
         restTemplate.exchange(studyServerBaseUri + path, HttpMethod.POST, request, Void.class);
     }
 
