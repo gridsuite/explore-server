aa760425a424186d090019d8998e258feea655d7
Seddik Yengui <seddik.yengui@rte-france.com>
2024-02-14 23:06:53 +0100

    revert changes

--- src/main/java/org/gridsuite/explore/server/ExploreController.java
+++ src/main/java/org/gridsuite/explore/server/ExploreController.java
@@ -250,7 +250,7 @@ public class ExploreController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping(value = "/explore/elements", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/explore/elements/metadata", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "get element infos from ids given as parameters")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "The elements information")})
     public ResponseEntity<List<ElementAttributes>> getElementsMetadata(@RequestParam("ids") List<UUID> ids,
