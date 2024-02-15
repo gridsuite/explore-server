f3770e66ea01954680eaa50966f0f60682c86ac8
jamal-khey <myjamal89@gmail.com>
2024-02-15 12:11:27 +0100

    add author and licence

--- src/main/java/org/gridsuite/explore/server/dto/CaseInfo.java
+++ src/main/java/org/gridsuite/explore/server/dto/CaseInfo.java
@@ -1,7 +1,15 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.explore.server.dto;
 
 import java.util.UUID;
-
+/**
+ * @author Jamal KHEYYAD <jamal.kheyyad at rte-france.com>
+ */
 public record CaseInfo(UUID caseUuid, String caseFormat) {
 
 }
