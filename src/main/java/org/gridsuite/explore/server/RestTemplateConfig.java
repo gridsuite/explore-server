1940fd839a9c6305a5ca7672e863dafd04f312ca
achour94 <berrahmaachour@gmail.com>
2025-05-15 10:48:46 +0200

    add role header propagation for cross-service requests
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

--- src/main/java/org/gridsuite/explore/server/RestTemplateConfig.java
+++ src/main/java/org/gridsuite/explore/server/RestTemplateConfig.java
@@ -8,13 +8,23 @@ package org.gridsuite.explore.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.jackson.JsonComponentModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.io.IOException;
+import java.util.Collections;
 
 /**
  * @author Etienne Homer <etienne.homer at rte-france.com>
@@ -32,9 +42,45 @@ public class RestTemplateConfig {
             if (httpMessageConverter instanceof MappingJackson2HttpMessageConverter) {
                 restTemplate.getMessageConverters().set(i, mappingJackson2HttpMessageConverter());
             }
+
+            restTemplate.setInterceptors(
+                    Collections.singletonList(new RoleHeaderForwardingInterceptor())
+            );
         }
 
         return restTemplate;
+    }
+
+    /**
+     * In our microservice architecture, user permissions (roles) must be preserved when
+     * one service calls another. This interceptor automatically forwards the "roles" header
+     * from incoming requests to any outgoing REST calls made with this RestTemplate.
+     *
+     * Without this interceptor, authorization information would be lost in service-to-service
+     * communication.
+     */
+    public static class RoleHeaderForwardingInterceptor implements ClientHttpRequestInterceptor {
+
+        private static final String ROLES_HEADER = "roles";
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest request, byte[] body,
+                                            ClientHttpRequestExecution execution) throws IOException {
+            ServletRequestAttributes attributes =
+                    (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+            // If we have a current request, copy its roles header to the outgoing request
+            if (attributes != null) {
+                HttpServletRequest currentRequest = attributes.getRequest();
+                String roles = currentRequest.getHeader(ROLES_HEADER);
+
+                if (roles != null && !roles.isEmpty()) {
+                    request.getHeaders().set(ROLES_HEADER, roles);
+                }
+            }
+
+            return execution.execute(request, body);
+        }
     }
 
     public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
