32d0e2bdff3bb76bea9b28723f45894b640587bf
Joris Mancini <joris.mancini_externe@rte-france.com>
2025-10-27 12:22:21 +0100

    fix(study-creation): ignore element not found in directory-server for direct creation
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/main/java/org/gridsuite/explore/server/services/ExploreService.java
+++ src/main/java/org/gridsuite/explore/server/services/ExploreService.java
@@ -6,6 +6,7 @@
  */
 package org.gridsuite.explore.server.services;
 
+import jakarta.annotation.Nullable;
 import org.apache.commons.lang3.StringUtils;
 import org.gridsuite.explore.server.error.ExploreException;
 import org.gridsuite.explore.server.dto.*;
@@ -13,7 +14,9 @@ import org.gridsuite.explore.server.utils.ContingencyListType;
 import org.gridsuite.explore.server.utils.ParametersType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.*;
@@ -87,13 +90,28 @@ public class ExploreService {
 
     public void createStudy(String studyName, CaseInfo caseInfo, String description, String userId, UUID parentDirectoryUuid, Map<String, Object> importParams, Boolean duplicateCase) {
         ElementAttributes elementAttributes = new ElementAttributes(UUID.randomUUID(), studyName, STUDY, userId, 0L, description);
-        // Two scenarios to handle.
-        // Scenario 1: the study is created from an existing case, so the case is available in the directory server.
-        // Scenario 2: the study is not created from an existing case, in which case the directory throws exception because no element with the given uuid.
-        ElementAttributes caseAttributes = directoryService.getElementInfos(caseInfo.caseUuid());
-        String elementName = caseAttributes.getElementName();
+
+        String elementName = getElementName(caseInfo.caseUuid());
+
         studyService.insertStudyWithExistingCaseFile(elementAttributes.getElementUuid(), userId, caseInfo.caseUuid(), caseInfo.caseFormat(), importParams, duplicateCase, elementName);
         directoryService.createElement(elementAttributes, parentDirectoryUuid, userId);
+    }
+
+    private @Nullable String getElementName(UUID elementUuid) {
+        String elementName = null;
+        // Two scenarios to handle.
+        try {
+            // Scenario 1: the study is created from an existing case, so the case is available in the directory server.
+            ElementAttributes caseAttributes = directoryService.getElementInfos(elementUuid);
+            elementName = caseAttributes.getElementName();
+        } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode() != HttpStatus.NOT_FOUND) {
+                throw e;
+            }
+            // Scenario 2: the study is not created from an existing case, in which case the directory throws exception
+            // because no element with the given uuid. Then, do nothing and keep elementName as null
+        }
+        return elementName;
     }
 
     public void duplicateStudy(UUID sourceStudyUuid, UUID targetDirectoryId, String userId) {
