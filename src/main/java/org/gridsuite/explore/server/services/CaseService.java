bf5f923f823b4d3fe5409d31a92ce65be2d1b4da
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2026-03-06 15:00:55 +0100

    Add support for persisting a case via the API (#175)

--- src/main/java/org/gridsuite/explore/server/services/CaseService.java
+++ src/main/java/org/gridsuite/explore/server/services/CaseService.java
@@ -57,6 +57,13 @@ public class CaseService implements IDirectoryElementsService {
         return caseUuid;
     }
 
+    void persistCase(UUID caseUuid) {
+        String path = UriComponentsBuilder.fromPath(DELIMITER + CASE_SERVER_API_VERSION + "/cases/" + caseUuid + "/disableExpiration")
+            .buildAndExpand()
+            .toUriString();
+        restTemplate.exchange(caseServerBaseUri + path, HttpMethod.PUT, new HttpEntity<>(new HttpHeaders()), void.class);
+    }
+
     UUID duplicateCase(UUID caseId) {
         String path = UriComponentsBuilder.fromPath(DELIMITER + CASE_SERVER_API_VERSION + "/cases")
             .queryParam("duplicateFrom", caseId)
